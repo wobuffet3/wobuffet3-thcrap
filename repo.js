b9fab32d083ea1808ebf89f1a6a9a865be27ec4a
nmlgc <nmlgc@nmlgc.net>
2017-09-15 07:38:21 +0200

    `"servers"` is not an object either. -.-

--- repo.js
+++ repo.js
@@ -1,9 +1,9 @@
 {
 	"contact": "wobuffet3@gmail.com",
 	"id": "wobuffet3",
-	"servers": {
+	"servers": [
 		"http://thcrap.nmlgc.net/repos/wobuffet3/",
 		"http://mirrors.thpatch.net/wobuffet3/"
-	}
+	]
 	"title": "Wobuffet3's patches"
 }
