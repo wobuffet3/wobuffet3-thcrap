c4cfad59a3b973323337e64f4ad7b3708680a7eb
nmlgc <nmlgc@nmlgc.net>
2017-09-15 07:39:39 +0200

    Run repo_update.py.

--- repo.js
+++ repo.js
@@ -1,9 +1,12 @@
 {
 	"contact": "wobuffet3@gmail.com",
 	"id": "wobuffet3",
+	"patches": {
+		"spaghetti": "Turns everything in HSiFS into spaghetti."
+	},
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/wobuffet3/",
 		"http://mirrors.thpatch.net/wobuffet3/"
-	]
+	],
 	"title": "Wobuffet3's patches"
 }
--- spaghetti/patch.js
+++ spaghetti/patch.js
@@ -4,5 +4,9 @@
 		"nmlgc/base_tasofro"
 	],
 	"id": "spaghetti",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/wobuffet3/spaghetti/",
+		"http://mirrors.thpatch.net/wobuffet3/spaghetti/"
+	],
 	"title": "Turns everything in HSiFS into spaghetti."
 }
