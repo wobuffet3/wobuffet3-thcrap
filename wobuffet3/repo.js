c4e0451c39aece33461144f555162f9d8f1b1459
wobuffet3 <kyu_bey@yahoo.com>
2017-09-15 01:17:36 -0400

    vb
    bbbbbbbbbb

---
+++ wobuffet3/repo.js
@@ -0,0 +1,9 @@
+{
+	"contact": "wobuffet3@gmail.com",
+	"id": "wobuffet3",
+	"servers": {
+		"http://thcrap.nmlgc.net/repos/wobuffet3/",
+		"http://mirrors.thpatch.net/wobuffet3/"
+	}
+	"title": "Wobuffet3's patches"
+}
---
+++ wobuffet3/spaghetti/patch.js
@@ -0,0 +1,8 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "spaghetti",
+	"title": "Turns everything in HSiFS into spaghetti."
+}
