d703a831d90252feba0cd53e6342102ec354a70e
wobuffet3 <kyu_bey@yahoo.com>
2018-10-09 14:34:16 -0400

    Update patch.js

--- sumija/patch.js
+++ sumija/patch.js
@@ -6,7 +6,7 @@
 	"id": "sumija",
 	"servers": [
 		"http://thcrap.nmlgc.net/repos/wobuffet3/sumija/",
-		"http://mirrors.thpatch.net/wobuffet3/sumija/",
+		"http://mirrors.thpatch.net/wobuffet3/sumija/"
 	],
 	"title": "Replaces Cirno and Aya with Sumireko and Seija."
 }
