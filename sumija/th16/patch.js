43935244e8971709c4315f651f89eba028bcc937
wobuffet3 <kyu_bey@yahoo.com>
2018-10-09 13:43:20 -0400

    Add files via upload

---
+++ sumija/th16/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "sumija",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/wobuffet3/sumija/",
+		"http://mirrors.thpatch.net/wobuffet3/sumija/"
+	],
+	"title": "Replaces Cirno and Aya with Sumireko and Seija."
+}
