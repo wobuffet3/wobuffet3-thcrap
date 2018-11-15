f423b7eede2efb31681dca6815270584addbb20d
wobuffet3 <kyu_bey@yahoo.com>
2018-11-15 14:15:26 -0500

    Add files via upload

---
+++ altplayers/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "altplayers",
+	"servers": [
+		"http://thcrap.nmlgc.net/repos/wobuffet3/altplayers/",
+		"http://mirrors.thpatch.net/wobuffet3/altplayers/"
+	],
+	"title": "Replaces player characters in Th16 with Sumireko, Seija, and Yabusame."
+}
