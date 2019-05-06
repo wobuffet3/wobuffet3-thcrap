fc93fdc103b97080f11978794a778f5aa66c130f
wobuffet3 <kyu_bey@yahoo.com>
2019-05-06 01:47:55 -0400

    Update repo.js

--- repo.js
+++ repo.js
@@ -2,8 +2,7 @@
 	"contact": "wobuffet3@gmail.com",
 	"id": "wobuffet3",
 	"servers": [
-		"http://thcrap.nmlgc.net/repos/wobuffet3/",
-		"http://mirrors.thpatch.net/wobuffet3/"
+		"https://mirrors.thpatch.net/wobuffet3/"
 	],
 	"title": "Wobuffet3's patches"
 }
