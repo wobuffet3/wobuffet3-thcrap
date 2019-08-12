60bf29bb0a68825750e111313426de56f4190e57
wobuffet3 <kyu_bey@yahoo.com>
2019-08-12 16:40:23 -0400

    Add files via upload

---
+++ visuals/patch.js
@@ -0,0 +1,11 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "spaghetti",
+	"servers": [
+		"https://mirrors.thpatch.net/wobuffet3/visuals"
+	],
+	"title": "Makes buffed shots in Th17 and season items in Th16 less distracting."
+}
