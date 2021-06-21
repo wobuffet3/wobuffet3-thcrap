676542cea825e33832360b56d425d1c9fa427675
wobuffet3 <kyu_bey@yahoo.com>
2021-06-21 17:50:46 -0400

    Update patch.js

--- visuals/patch.js
+++ visuals/patch.js
@@ -7,5 +7,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/wobuffet3/visuals"
 	],
-	"title": "(TH16, 17, 18) Makes Sanae's shot in Th18, roaring shots in Th17, and season items in Th16 less distracting. Included with spaghetti."
+	"title": "(TH16, 17, 18) Makes various shots in Th18, roaring shots in Th17, and season items in Th16 less distracting. Included with spaghetti."
 }
