c8a4f31a036bf858447f8358dc5876042e22782f
wobuffet3 <kyu_bey@yahoo.com>
2021-08-25 12:38:48 -0400

    Update patch.js

--- spaghetti/patch.js
+++ spaghetti/patch.js
@@ -8,5 +8,5 @@
 		"http://thcrap.nmlgc.net/repos/wobuffet3/spaghetti/",
 		"http://mirrors.thpatch.net/wobuffet3/spaghetti/"
 	],
-	"title": "(TH16, 16.5, 17)Turns everything into spaghetti. Contains 'visuals' patch."
+	"title": "(TH16, 16.5, 17) Turns everything into spaghetti. Contains 'visuals' patch."
 }
