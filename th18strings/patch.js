b7fde49fa69ed91cb048cca8ac2a4a1b70f470af
wobuffet3 <kyu_bey@yahoo.com>
2021-09-13 16:31:49 -0400

    Add files via upload

---
+++ th18strings/patch.js
@@ -0,0 +1,11 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa",
+    "nmlgc/base_tasofro"
+  ],
+  "id": "th18strings",
+  "servers": [
+    "https://mirrors.thpatch.net/wobuffet3/th18strings/"
+  ],
+  "title": "(TH18) Slightly enhances Th18's music by adding strings, making songs sound less empty. Also enhances Saki's theme in Th17."
+}
