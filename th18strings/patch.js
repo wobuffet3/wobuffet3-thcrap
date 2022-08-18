19ed6213db5ee558ae3b9b77d87d67a88c474300
wobuffet3 <kyu_bey@yahoo.com>
2022-08-18 14:14:58 -0400

    Update patch.js

--- th18strings/patch.js
+++ th18strings/patch.js
@@ -7,5 +7,5 @@
   "servers": [
     "https://mirrors.thpatch.net/wobuffet3/th18strings/"
   ],
-  "title": "(TH18) Slightly enhances Th18's music by adding strings, making songs sound less empty. Also enhances Saki's theme in Th17."
+  "title": "(TH18, 18.5) Slightly enhances Th18's music by adding strings, making songs sound less empty. Also enhances Saki's theme in Th17 and the reused Th18 songs in Th18.5."
 }
