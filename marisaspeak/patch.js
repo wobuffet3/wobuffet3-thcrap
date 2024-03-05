94ce5db3521152cc4bf0cbf13b48f4db27444682
wobuffet3 <kyu_bey@yahoo.com>
2024-03-04 23:11:41 -0500

    Update patch.js

--- marisaspeak/patch.js
+++ marisaspeak/patch.js
@@ -7,5 +7,5 @@
   "servers": [
     "https://mirrors.thpatch.net/wobuffet3/spaghetti/"
   ],
-  "title": "(TH12+) Reverts Marisa's cowboy dialect. ZUN games only for now."
+  "title": "WIP (TH12-13) Reverts Marisa's cowboy dialect. ZUN games only for now."
 }
