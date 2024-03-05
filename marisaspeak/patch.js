c4404487aa99352c3cc4177e0e773b1669151c66
wobuffet3 <kyu_bey@yahoo.com>
2024-03-04 22:09:45 -0500

    Add files via upload

---
+++ marisaspeak/patch.js
@@ -0,0 +1,11 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa",
+    "nmlgc/base_tasofro"
+  ],
+  "id": "marisaspeak",
+  "servers": [
+    "https://mirrors.thpatch.net/wobuffet3/spaghetti/"
+  ],
+  "title": "(TH12+) Reverts Marisa's cowboy dialect. ZUN games only for now."
+}
