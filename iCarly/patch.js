929c6aa39ddba65352d4ef70519ebcdaa8a9b05d
wobuffet3 <kyu_bey@yahoo.com>
2024-09-04 17:01:12 -0400

    Add files via upload

---
+++ iCarly/patch.js
@@ -0,0 +1,11 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa",
+    "nmlgc/base_tasofro"
+  ],
+  "id": "iCarly",
+  "servers": [
+    "https://mirrors.thpatch.net/wobuffet3/icarly/"
+  ],
+  "title": "(TH08) Replaces Border Team with Carly and Sam. Full story replacement."
+}
