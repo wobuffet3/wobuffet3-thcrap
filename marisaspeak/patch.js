7a77ff49b63ad8c0cc3b1b8b5773603d31c3642c
wobuffet3 <kyu_bey@yahoo.com>
2024-09-04 16:58:12 -0400

    Update patch.js
    WHOOPS

--- marisaspeak/patch.js
+++ marisaspeak/patch.js
@@ -5,7 +5,7 @@
   ],
   "id": "marisaspeak",
   "servers": [
-    "https://mirrors.thpatch.net/wobuffet3/spaghetti/"
+    "https://mirrors.thpatch.net/wobuffet3/marisaspeak/"
   ],
   "title": "(TH12-19) Reverts Marisa's cowboy dialect."
 }
