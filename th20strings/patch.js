031a7d49162186093f973e845d4b27118ef0d2e3
wobuffet3 <kyu_bey@yahoo.com>
2026-03-12 12:18:48 -0400

    Add files via upload

---
+++ th20strings/patch.js
@@ -0,0 +1,11 @@
+{
+  "dependencies": [
+    "nmlgc/base_tsa",
+    "nmlgc/base_tasofro"
+  ],
+  "id": "th20strings",
+  "servers": [
+    "https://mirrors.thpatch.net/wobuffet3/th20strings/"
+  ],
+  "title": "(TH20, incomplete) Enhances Th20's music by making some songs sound less empty."
+}
