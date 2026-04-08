f68dfd25ea1361f5b3a574f626daed51363c5684
wobuffet3 <kyu_bey@yahoo.com>
2026-04-07 20:27:49 -0400

    Update patch.js

--- th20strings/patch.js
+++ th20strings/patch.js
@@ -7,5 +7,5 @@
   "servers": [
     "https://mirrors.thpatch.net/wobuffet3/th20strings/"
   ],
-  "title": "(TH20, incomplete) Enhances Th20's music by making some songs sound less empty."
+  "title": "(TH20) Enhances Th20's music by making some songs sound less empty."
 }
